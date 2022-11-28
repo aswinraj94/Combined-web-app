b1e6a98c60f82396d6937e81193f7f5454c6f4c9
aswinraj94 <aswinraj94@gmail.com>
2022-11-28 15:38:53 +0000

    combined with no type script

---
+++ hardhat.config.js
@@ -0,0 +1,32 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config({ path: ".env" });
+
+const QUICKNODE_HTTP_URL = process.env.QUICKNODE_HTTP_URL;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+
+module.exports = {
+  solidity:  {
+    compilers: [
+      {
+        version: "0.8.9",
+      },
+      {
+        version: "0.4.24",
+        settings: {},
+      },
+      {
+        version: "0.5.16",
+        settings: {},
+      },
+      {
+        version: "0.8.13",
+      },
+    ],
+  },
+  networks: {
+    goerli: {
+      url: QUICKNODE_HTTP_URL,
+      accounts: [PRIVATE_KEY],
+    },
+  },
+};
